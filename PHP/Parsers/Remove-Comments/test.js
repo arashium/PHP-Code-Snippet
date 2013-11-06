b9c9c3ce376679d4a5f59144e3cc69e192bf2704
Arash <arashium@yahoo.com>
2013-11-06 23:24:32 +0330

    Create test.js

---
+++ PHP/Parsers/Remove-Comments/test.js
@@ -0,0 +1,10 @@
+/******************************************
+* This is a multi-line comment
+*/
+
+function relative_datetime(current, previous)
+{
+	alert('inside quotation marks');
+	// inline comment
+	alert('inside double quotation marks');
+}
